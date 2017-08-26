eb47f1901f74b104f03267458f6604a68f261335
xiedacon <xiedacon@qq.com>
2017-08-26 17:40:04 +0800

    refact(git): refact git plugin

--- plugins/git.js
+++ plugins/git.js
@@ -1,19 +1,22 @@
 'use strict'
 
-const { spawnSync } = require('child_process')
-
-const encoding = 'utf8'
+const { execSync } = require('child_process')
 
 module.exports = (opts) => {
-  spawnSync('git', ['init'])
-  spawnSync('git', ['add', '.'])
-  spawnSync('git', ['commit', '-m', 'init by https://github.com/xiedacon/generate'])
-  spawnSync('git', ['remote', 'add', 'origin', `https://github.com/${opts.user}/${opts.repository}.git`])
-  let result = spawnSync('git', ['push', '-u', 'origin', 'master'], { encoding })
-  if (result.status !== 0) {
-    console.error(result.stderr)
-    throw new Error(result.stderr)
-  } else {
-    console.log(result.stdout)
+  let { encoding } = opts
+  try {
+    let result = execSync(`
+      git init
+      git add .
+      git commit -m 'init by https://github.com/xiedacon/generate'
+      git remote add origin https://github.com/${opts.user}/${opts.repository}.git
+      git push -u orgin master
+    `, { encoding, cwd: process.cwd(), stdio: [] })
+
+    console.log(result)
+  } catch (err) {
+    if (err.toString().indexOf('远程 origin 已经存在') < 0) throw err
+
+    execSync('git push --set-upstream origin master', { encoding, cwd: process.cwd() })
   }
 }
