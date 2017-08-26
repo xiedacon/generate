1b766c0606cc6e8646aa2d33682a13166e268820
xiedacon <xiedacon@qq.com>
2017-08-26 16:56:06 +0800

    feature: add npm plugin

---
+++ plugins/npm.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const { execSync } = require('child_process')
+
+module.exports = (config) => {
+  let encoding = config.encoding
+
+  console.log(execSync('npm install', { encoding, cwd: process.cwd() }))
+}
