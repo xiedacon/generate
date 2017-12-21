5b179b39003120179f83e82509face8998252655
xiedacon <xiedacon@qq.com>
2017-12-21 23:45:14 +0800

    chore(plugins): 取消自动 push

--- plugins/git.js
+++ plugins/git.js
@@ -4,19 +4,13 @@ const { execSync } = require('child_process')
 
 module.exports = (opts) => {
   let { encoding } = opts
-  try {
-    let result = execSync(`
-      git init
-      git add .
-      git commit -m 'init by https://github.com/xiedacon/generate'
-      git remote add origin https://github.com/${opts.user}/${opts.repository}.git
-      git push -u orgin master
-    `, { encoding, cwd: process.cwd(), stdio: [] })
 
-    console.log(result)
-  } catch (err) {
-    if (err.toString().indexOf('远程 origin 已经存在') < 0) throw err
+  let result = execSync(`
+    git init
+    git add .
+    git commit -m 'init by https://github.com/xiedacon/generate'
+    git remote add origin https://github.com/${opts.user}/${opts.repository}.git
+  `, { encoding, cwd: process.cwd(), stdio: [] })
 
-    execSync('git push --set-upstream origin master', { encoding, cwd: process.cwd() })
-  }
+  console.log(result)
 }
